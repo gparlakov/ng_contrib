8e7261cfc2da11422ffb74b2b1ed51f60dc4bd4e
Georgi Parlakov <gparlakov@gmail.com>
2018-06-10 20:34:17 +0300

    Debugging angular in vs code

--- go.cmd
+++ go.cmd
@@ -1,1 +1,1 @@
-docker run --rm -ti  -v %~dp0\\angular:/angular -p 9876:9876 -p9229:9229 --name ng_contrib  node:8 bash
+docker run --rm -ti  -v %~dp0\\angular:/angular --name ng_contrib  node:8 bash
